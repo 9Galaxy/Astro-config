603de92c552fabb618d05f8b656c7ce7e7e56871
9Galaxy <marianomarcelo13@gmail.com>
2024-03-05 01:24:56 -0500

    Codeium Integrated

--- lua/community.lua
+++ lua/community.lua
@@ -6,23 +6,58 @@
 return {
   -- TODO: Remove branch v4 on release
   --
+  --
+  -- Animation Scrolling
+
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+
+  -- Code-Runner
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.code-runner.compiler-nvim" },
+
+  -- Completion
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.completion.coq_nvim" },
+
+  -- Color
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.color.ccc-nvim" },
+
   --ColorScheme
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.colorscheme.nightfox-nvim" },
 
+  -- Debugging
   { "AstroNvim/astrocommunity", branch = "v4" },
-  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
+
+  -- Diagnostics
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+
+  -- Editing-support
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+
+  -- Motion
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.motion.flash-nvim" },
   -- Pack Languages-Support
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.astro" },
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.html-css" },
   { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.pack.json" },
+  { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.lua" },
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.markdown" },
   { "AstroNvim/astrocommunity", branch = "v4" },
-  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.python-ruff" },
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.pack.tailwindcss" },
   { "AstroNvim/astrocommunity", branch = "v4" },
@@ -34,5 +69,13 @@ return {
   { import = "astrocommunity.recipes.vscode-icons" },
   { "AstroNvim/astrocommunity", branch = "v4" },
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+
+  -- Search
+  { "AstroNvim/astrocommunity", branch = "v4" },
+  { import = "astrocommunity.search.sad-nvim" },
+
+  -- -- Testing
+  -- { "AstroNvim/astrocommunity", branch = "v4" },
+  -- { import = "astrocommunity.test.neotest" },
 }
 -- import/override with your plugins folder
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize None-ls sources
 
 ---@type LazySpec
@@ -8,25 +6,25 @@ return {
   opts = function(_, config)
     -- config variable is the default configuration table for the setup function call
     local null_ls = require "null-ls"
-    local augroup = vim.api.nvim_create_augroup "LSPFormatting, {}"
-
-    config.server {
-
-      on_attach = function(client, bufnr)
-        if client.supports_method "textDocument/formatting" then
-          vim.api.nvim_clear_autocmds {
-            group = augroup,
-            buffer = bufnr,
-          }
-          vim.api.nvim_create_autocmd("BufWritePre", {
-
-            group = augroup,
-            buffer = bufnr,
-            callback = function() vim.lsp.buf.format { bufnr = bufnr } end,
-          })
-        end
-      end,
-    }
+    -- local augroup = vim.api.nvim_create_augroup "LSPFormatting, {}"
+    --
+    -- config.server {
+    --
+    --   on_attach = function(client, bufnr)
+    --     if client.supports_method "textDocument/formatting" then
+    --       vim.api.nvim_clear_autocmds {
+    --         group = augroup,
+    --         buffer = bufnr,
+    --       }
+    --       vim.api.nvim_create_autocmd("BufWritePre", {
+    --
+    --         group = augroup,
+    --         buffer = bufnr,
+    --         callback = function() vim.lsp.buf.format { bufnr = bufnr } end,
+    --       })
+    --     end
+    --   end,
+    -- }
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
