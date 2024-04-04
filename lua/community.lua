1dc7b43a1ffd163e1f6a60de501579e4878e5098
9Galaxy <marianomarcelo13@gmail.com>
2024-04-03 20:00:56 -0500

    V4 Released

--- lua/community.lua
+++ lua/community.lua
@@ -4,90 +4,89 @@
 
 ---@type LazySpec
 return {
-  -- TODO: Remove branch v4 on release
   --
   --
   -- Animation Scrolling
 
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.scrolling.mini-animate" },
 
   -- Code-Runner
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.code-runner.compiler-nvim" },
 
   -- Completion
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.completion.codeium-vim" },
 
   --BUG: This plugin doesn't work for web-development. Remove this if you find some alternatives or use it if you are gonna work with python.
-  -- { "AstroNvim/astrocommunity", branch = "v4" },
+  -- { "AstroNvim/astrocommunity",  },
   -- { import = "astrocommunity.completion.coq_nvim" },
 
   -- Color
 
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.color.ccc-nvim" },
 
   --ColorScheme
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.colorscheme.nightfox-nvim" },
 
   -- Debugging
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.debugging.nvim-dap-virtual-text" },
 
   -- Diagnostics
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
 
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.diagnostics.trouble-nvim" },
 
   -- Editing-support
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.editing-support.multicursors-nvim" },
 
   -- Motion
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.motion.flash-nvim" },
   -- Pack Languages-Support
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.astro" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.html-css" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.json" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.markdown" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.python-ruff" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.tailwindcss" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.typescript" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.toml" },
 
   --Project
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.project.nvim-spectre" },
 
   -- Recipes
 
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.vscode-icons" },
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
 
   -- Search
-  { "AstroNvim/astrocommunity", branch = "v4" },
+  "AstroNvim/astrocommunity",
   { import = "astrocommunity.search.sad-nvim" },
 
   -- -- Testing
-  -- { "AstroNvim/astrocommunity", branch = "v4" },
-  -- { import = "astrocommunity.test.neotest" },
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.test.neotest" },
 }
 -- import/override with your plugins folder
